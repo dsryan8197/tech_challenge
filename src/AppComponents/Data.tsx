51593bc0d795c39723066dcc2c3a3910c0471672
Seamus <d.seamus.ryan@outlook.com>
2021-01-14 21:12:05 -0600

    fetch request for songs works

--- src/AppComponents/Data.tsx
+++ src/AppComponents/Data.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+const axios = require('axios')
 // import { Link } from 'react-router-dom'
 // import { SideNavTabs } from '../components/SideNavTabs'
-
+  
 const Data: React.FC = () => {
+  const [songs, setSongs] = useState('')
+  const [temp, setTemp]= useState('')
+
+  useEffect(() => {
+    axios('https://iheartradiotechchallenge.s3.us-east-2.amazonaws.com/songData.json')
+    .then((res:any)=> {
+      console.log(res.data)
+    })
+    .catch((err:any)=> {
+      console.log(err)
+    })
+  },[songs])
+
+
   return (
     <div className='dataDiv'>
       <h1>Data here</h1>
+      <button onClick={()=>setTemp('hi')}>hi</button>
     </div>
   );
 }
+
 
 export default Data;
 
