d7666b1d5fd8bf7b17cef0ee9c30441ddf53fe84
Seamus <d.seamus.ryan@outlook.com>
2021-01-14 22:23:58 -0600

    table rendering all data

--- src/AppComponents/Data.tsx
+++ src/AppComponents/Data.tsx
@@ -4,24 +4,51 @@ const axios = require('axios')
 // import { SideNavTabs } from '../components/SideNavTabs'
   
 const Data: React.FC = () => {
-  const [songs, setSongs] = useState('')
-  const [temp, setTemp]= useState('')
+  const [songInfo, setSongInfo] = useState([])
 
   useEffect(() => {
     axios('https://iheartradiotechchallenge.s3.us-east-2.amazonaws.com/songData.json')
     .then((res:any)=> {
-      console.log(res.data)
+      setSongInfo(res.data)
+      console.log(songInfo)
     })
     .catch((err:any)=> {
       console.log(err)
     })
-  },[songs])
+  }, [])
 
+  const createTable = (songInfo:any) => {
+    let arr = []
+    for (let i = 0; i < songInfo.length; i++) {
+      arr.push(
+          <tr key={i}>
+            <td>{songInfo[i].song}</td>
+            <td>{songInfo[i].artist}</td>
+            <td>{songInfo[i].songReleaseDate}</td>
+            <td>{songInfo[i].playCount}</td>
+          </tr>
+      )
+    }
+    console.log(arr)
+    return arr
+  }
 
   return (
     <div className='dataDiv'>
       <h1>Data here</h1>
-      <button onClick={()=>setTemp('hi')}>hi</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Date</th>
+            <th>Play setCount</th>
+          </tr>
+        </thead>
+        <tbody>
+           {createTable(songInfo)}
+        </tbody>
+      </table>
     </div>
   );
 }
